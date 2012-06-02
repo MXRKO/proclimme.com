20bf383639c5ffced84d97e92f815dc5eb7fc276
Marco Antonio Lozada <zero_could00@hotmail.com>
2012-06-02 00:09:18 -0500

    version1
    Revisado antes del entregable del sábado

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PROCLIMME</title>
+<link href="lib/css/reset.css" rel="stylesheet" type="text/css" />
+<link href="lib/css/diseno.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="top">
+     <div class="centrar">
+        <div class="logo">
+        </div>
+        <div class="dMenu">
+            <ul class="menu">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Costos</a></li>
+                <li><a href="#">Quienes somos</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>    
+        </div>
+        <div class="limpiar"></div>    
+     </div>
+</div>
+<div class="imgCentral">
+    	<div class="bannerImgs">
+        	<div class="bannerItem uno">
+            <p class="subtitulo">Productos climatologicos y meteorologicos de México</p>
+            <p class="slogan">&quot;El clima y el tiempo de tu lado&quot;</p>
+        	<input id="sesion" type="image" src="image/btnSesion.png" />
+        	</div>
+        </div>
+    </div>
+<div class="centro">
+	<div class="miniaturas">
+    	<ul class="min">
+        	<li><a href="#"><img src="media/productos/miniatura3.png" width="89" height="64" /></a></li>
+            <li><a href="#"><img src="media/productos/miniatura5.png" width="89" height="64" /></a></li>
+            <li><a href="#"><img src="media/productos/miniatura6.png" width="89" height="64" /></a></li>
+            <li><a href="#"><img src="media/productos/miniatura7.png" width="89" height="64" /></a></li>
+            <li><a href="#"><img src="media/productos/miniatura9.png" width="89" height="64" /></a></li>
+            <li><a href="#"><img src="media/productos/miniatura4.png" width="89" height="64" /></a></li>
+            <li><a href="#"><img src="media/productos/miniatura8.png" width="89" height="64" /></a></li>
+        </ul>
+        <div class="limpiar"></div>
+    </div>
+	<div class="bannerPrincipal">	
+        <div class="productos">
+            <div class="item">
+            	<div class="imgItem">
+                	<img src="media/productos/item3.png" height="399" width="606" />
+                </div>
+                <div class="datosItem">
+                	<h1>Pronóstico meteorológico a 3 días</h1>
+                    <p>Pronostico de variables y eventos meteorológicos significativos que puedan presentarse dentro de las próximas 72 horas.</p>
+                </div>	
+                <div class="limpiar"></div>
+            </div>
+            <div class="item">
+            	<div class="imgItem">
+                	<img src="media/productos/item5.png" height="399" width="606" />
+                </div>
+                <div class="datosItem">
+                    <h1>Pronóstico climatológico estacional</h1>
+                    <p>Pronóstico probabilístico de precipitación, temperatura y otras variables climatológicas para 6 o más meses, enfocadas a determinar las condiciones esperadas con respecto a valores normales.</p>
+                </div>	
+                <div class="limpiar"></div>
+            </div>
+            <div class="item">
+            	<div class="imgItem">
+                	<img src="media/productos/item6.png" height="399" width="606" />
+                </div>
+                <div class="datosItem">
+                    <h1>Pronóstico climatológico estacional</h1>
+                    <p>Pronóstico probabilístico de precipitación, temperatura y otras variables climatológicas para 6 o más meses, enfocadas a determinar las condiciones esperadas con respecto a valores normales.</p>
+                </div>	
+                <div class="limpiar"></div>
+            </div>
+            <div class="item">
+            	<div class="imgItem">
+                	<img src="media/productos/item7.png" height="399" width="606" />
+                </div>
+                <div class="datosItem">
+                    <h1>Pronóstico climatológico estacional</h1>
+                    <p>Pronóstico probabilístico de precipitación, temperatura y otras variables climatológicas para 6 o más meses, enfocadas a determinar las condiciones esperadas con respecto a valores normales.</p>
+                </div>	
+                <div class="limpiar"></div>
+            </div>
+            <div class="item">
+            	<div class="imgItem">
+                	<img src="media/productos/item9.png" height="399" width="606" />
+                </div>
+                <div class="datosItem">
+                    <h1>Pronóstico climatológico estacional</h1>
+                    <p>Pronóstico probabilístico de precipitación, temperatura y otras variables climatológicas para 6 o más meses, enfocadas a determinar las condiciones esperadas con respecto a valores normales.</p>
+                </div>	
+                <div class="limpiar"></div>
+            </div>
+            <div class="item">
+            	<div class="imgItem">
+                	<img src="media/productos/item4.png" height="399" width="606" />
+                </div>
+                <div class="datosItem">
+                    <h1>Pronóstico climatológico estacional</h1>
+                    <p>Pronóstico probabilístico de precipitación, temperatura y otras variables climatológicas para 6 o más meses, enfocadas a determinar las condiciones esperadas con respecto a valores normales.</p>
+                </div>	
+                <div class="limpiar"></div>
+            </div>
+            <div class="item">
+            	<div class="imgItem">
+                	<img src="media/productos/item8.png" height="399" width="606" />
+                </div>
+                <div class="datosItem">
+                    <h1>Pronóstico climatológico estacional</h1>
+                    <p>Pronóstico probabilístico de precipitación, temperatura y otras variables climatológicas para 6 o más meses, enfocadas a determinar las condiciones esperadas con respecto a valores normales.</p>
+                </div>	
+                <div class="limpiar"></div>
+            </div>
+        </div>        
+	</div>
+</div>
+<div class="contenido">
+	<div class="marco">
+    	<div class="noticias">
+        	<div class="encabezado"><h2>Noticias</h2></div>
+            <div class="marco">
+                <p class="titulo">Imágenes de la Tierra en HD</p>
+                <div class="imgNoticia"></div>
+                <p class="fecha">27-05-2012</p>
+                <p class="descripcion">Imágenes de la Tierra en alta resolución tomadas por el satélite Ruso Electro-L en distintas longitudes de onda</p>
+            </div>
+            <div class="marco">
+                <p class="titulo">Radiación solar intensa y estabilidad atmosférica propician una disminución en la calidad del aire para el D.F</p>
+                <div class="imgNoticia"></div>
+                <p class="fecha">24-05-2012</p>
+                <p class="descripcion">El Sistema de Monitoreo Atmosférico de la Ciudad de México (Simat) informó que la radiación solar es extremadamente alta en el valle de México, de 11 puntos el Índice de Rayos Ultravioleta (UV).</p>
+            </div>
+            <div class="marco">
+                <p class="titulo">Fuertes vientos asociados a tormenta causan diversos daños en el distrito federal</p>
+                <div class="imgNoticia"></div>
+                <p class="fecha">21-05-2012</p>
+                <p class="descripcion">Tres árboles cayeron por los fuertes vientos y la ligera llovizna, dejando dos vehículos dañados y toda una colonia sin luz en la Gustavo A. Madero</p>
+                <div class="limpiar"> </div>
+            </div>
+        </div>
+        <div class="links"><p><img src="image/mac.png" width="305" height="251" />dffdsfsdfds</p></div>
+    	<div class="limpiar"></div>
+    </div>
+</div>
+<div class="footer">
+	<p>PROCLIMME, Xalapa, Veracruz, México, 2012</p>
+</div>
+</body>
+<script type="text/javascript" language="javascript" src="lib/js/jquery-1.5.2.min.js"></script>
+<script type="text/javascript" language="javascript" src="lib/js/jquery.cycle.all.js"></script>
+<script>
+	$(function(){
+		$('.productos').cycle({ 
+			fx:      'fade', 
+			speed:    400, 
+			slices: 5,
+			timeout:  3500,
+			pause:		1,
+			pager: 'ul.min',
+			pagerAnchorBuilder: function(idx, slide) {
+				// return selector string for existing anchor
+				return 'ul.min li:eq(' + idx + ') a';
+			}
+		});
+	});
+	
+	$(document).ready(function(){
+		$("#sesion").click(function(){
+			alert("En desarrollo");							
+		});						   
+	});
+</script>
+</html>
