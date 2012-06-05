b2f223543587aabfa996e74b8d8494cecdd4227b
Marco Lozada <zero_could00@hotmail.com>
2012-06-05 18:24:58 -0500

    Se creo base de seccion complementaria
    Se creo la base para las demás secciones, se empezo por sección quienes
    somos

---
+++ quienes.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PROCLIMME</title>
+<link href="lib/css/reset.css" rel="stylesheet" type="text/css" />
+<link href="lib/css/diseno.css" rel="stylesheet" type="text/css" />
+<link href="lib/css/tinybox/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="top">
+     <div class="centrar">
+        <div class="logo">
+        </div>
+        <div class="dMenu">
+            <ul class="menu">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Costos</a></li>
+                <li><a href="#">Quienes somos</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>    
+        </div>
+        <div class="limpiar"></div>    
+     </div>
+</div>
+<div class="imgCentral">
+    <div class="bannerImgs">
+        <div class="bannerItem quienes">
+        <h2>Quienes</h2>
+        </div>
+    </div>
+</div>
+<div class="subbarra">
+	<div class="marco">
+    	<p>breve descripción</p>
+    </div>
+</div>
+<div class="contenido">
+	<div class="marco">
+    	<!-- <a href="media/video/intro_proclimme.flv"
+   style="display:block;width:405px;height:300px;"
+   id="player">
+</a> -->
+    <p>Texto </p>
+    </div>
+</div>
+<div class="footer">
+	<div class="marco">
+    	<div class="logo_footer">
+        	<p><img src="image/logo_footer.png" height="33" width="172"/></p>
+    	</div>
+        <div class="menufooter">
+        	<ul>
+            	<li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Costos</a></li>
+                <li><a href="#">Quienes somos</a></li>
+                <li><a href="#">Contctanos</a></li>
+            </ul>
+        </div>
+        <div class="limpiar"></div>
+    </div>
+</div>
+</body>
+<script type="text/javascript" language="javascript" src="lib/js/jquery-1.5.2.min.js"></script>
+<script type="text/javascript" language="javascript" src="lib/js/jquery.cycle.all.js"></script>
+<script type="text/javascript" language="javascript" src="lib/js/tinybox2/tinybox.js"></script>
+</html>
