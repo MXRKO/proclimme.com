caebf67e777633effb0110969c507a3a1ea8bb43
Marco Lozada <zero_could00@hotmail.com>
2012-06-13 18:53:04 -0500

    trabajando carrito de compra
    Se esta trabajando el carrito de compra de los productos

---
+++ producto.php
@@ -0,0 +1,88 @@
+<?
+	session_start();
+	include("lib/php/settings.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PROCLIMME</title>
+<link href="lib/css/reset.css" rel="stylesheet" type="text/css" />
+<link href="lib/css/diseno.css" rel="stylesheet" type="text/css" />
+<link href="lib/css/producto.css" rel="stylesheet" type="text/css" />
+<link href="lib/css/tinybox/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="misesion">
+	<div class="opciones">
+    	<ul class="ulUser">
+        	<li class="ultimo"><a class="ultimo" href="#">Salir</a></li>
+            <li><a class="carrito" href="#">Mi pedido (0)</a></li>
+            <li class="primero"><a class="primero" href="#">Mi perfil</a></li>
+        </ul>
+    </div>
+</div>
+<div class="top sesionactiva">
+  <div class="centrar">
+        <div class="logo">
+        </div>
+        <div class="dMenu">
+            <ul class="menu">
+                <li><a href="<?=$menu["inicio"]?>">Inicio</a></li>
+                <li><a href="<?=$menu["productos"]?>">Productos</a></li>
+                <li><a href="<?=$menu["costos"]?>">Costos</a></li>
+                <li><a href="#">Quienes somos</a></li>
+                <li><a href="<?=$menu["contacto"]?>">Contacto</a></li>
+            </ul>    
+        </div>
+        <div class="limpiar"></div>    
+     </div>
+</div>
+<!-- <div class="subbarra">
+	<div class="marco">
+    	<p>breve descripción</p>
+    </div>
+</div> -->
+
+<div class="contenido">
+	<div class="encabezado">
+    	<div class="datosIzq">
+        	<p>Prueba</p>
+        </div>
+        <div class="datosDer">
+        	<h1>Nombre del producto de este apartado</h1>
+            <p><strong>Datos básicos: </strong>Nunc nunc amet, dis quis, non est mus urna! Eros, pulvinar quis elementum nascetur dis? Sed diam elementum. In? Turpis mattis habitasse magna porta mattis lorem odio! Tortor aliquam placerat eros placerat arcu, phasellus nunc, dolor a montes! Tincidunt.</p>
+        </div>
+    </div>
+	<div class="descripcionProducto">
+        <h1>Descripción detallada del producto</h1>
+        <p>Somos un grupo de trabajo especializado en Climatología e Hidrometeorología que ofrece productos enfocados al análisis, estudio, monitoreo y previsión de fenómenos atmosféricos y climáticos, así como sus repercusiones, con el fin de reducir los impactos negativos que puedan ocasionar, mejorar las capacidades de planeación ante su ocurrencia y constituir un apoyo en la toma de decisiones importantes para distintos rubros en la sociedad.</p>
+        <p>Nuestros productos son el resultado de análisis estadísticos y modelación numérica de la atmósfera que permiten acceder a información climática y meteorológica de manera actualizada con el fin de proveer condiciones climatológicas futuras, considerando fenómenos y eventos a distintas escalas espaciales y temporales como sistemas convectivos de mesoescala, ciclones tropicales, oscilaciones planetarias y multidecadales, el fenómeno de “El Niño” entre otros.</p>
+        
+        <div class="social">
+        	<input type="image" src="image/btnFace.png" onclick="window.location.href='https://www.facebook.com/Proclimme'" /><input type="image" src="image/btnYouTube.png" onclick="window.location.href='http://www.youtube.com/channel/UCISegxqV_mwSbUSPw0DrGzw?feature=results_main'" />
+        </div>    
+    </div>
+</div>
+<div class="footer">
+	<div class="marco">
+    	<div class="logo_footer">
+        	<p><img src="image/logo_footer.png" height="24" width="122"/></p>
+    	</div>
+        <div class="menufooter">
+        	<ul>
+            	<li><a href="<?=$menu["inicio"]?>">Inicio</a></li>
+                <li><a href="<?=$menu["productos"]?>">Productos</a></li>
+                <li><a href="<?=$menu["costos"]?>">Costos</a></li>
+                <li><a href="#">Quienes somos</a></li>
+                <li><a href="<?=$menu["contacto"]?>">Contacto</a></li>
+            </ul>
+        </div>
+        <div class="limpiar"></div>
+    </div>
+</div>
+</body>
+<script type="text/javascript" language="javascript" src="lib/js/jquery-1.5.2.min.js"></script>
+<script type="text/javascript" language="javascript" src="lib/js/jquery.cycle.all.js"></script>
+<script type="text/javascript" language="javascript" src="lib/js/tinybox2/tinybox.js"></script>
+</html>
---
+++ src/proclimme_db.sql
@@ -0,0 +1,101 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.13 
+MySQL - 5.0.67-community : Database - proclimme_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`proclimme_db` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+
+USE `proclimme_db`;
+
+/*Table structure for table `clientes` */
+
+DROP TABLE IF EXISTS `clientes`;
+
+CREATE TABLE `clientes` (
+  `id` int(12) default NULL,
+  `id_usuario` int(12) default NULL,
+  `nombre` varchar(255) collate utf8_spanish_ci default NULL,
+  `apellidos` varchar(255) collate utf8_spanish_ci default NULL,
+  `direccion` varchar(450) collate utf8_spanish_ci default NULL,
+  `rfc` varchar(21) collate utf8_spanish_ci default NULL,
+  `telefono_casa` varchar(25) collate utf8_spanish_ci default NULL,
+  `telefono_celular` varchar(25) collate utf8_spanish_ci default NULL,
+  `telefono_trabajo` varchar(25) collate utf8_spanish_ci default NULL,
+  `email` varchar(55) collate utf8_spanish_ci default NULL,
+  `fax` varchar(25) collate utf8_spanish_ci default NULL,
+  `no_cuenta` varchar(25) collate utf8_spanish_ci default NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `clientes` */
+
+/*Table structure for table `imagenes` */
+
+DROP TABLE IF EXISTS `imagenes`;
+
+CREATE TABLE `imagenes` (
+  `id` int(12) NOT NULL auto_increment,
+  `id_producto` int(12) default NULL,
+  `nombre_archivo` varchar(450) collate utf8_spanish_ci default NULL,
+  `descripcion` text collate utf8_spanish_ci,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `imagenes` */
+
+/*Table structure for table `pedidos` */
+
+DROP TABLE IF EXISTS `pedidos`;
+
+CREATE TABLE `pedidos` (
+  `id` int(12) NOT NULL auto_increment,
+  `id_producto` int(12) default NULL,
+  `id_usuario` int(12) default NULL,
+  `fecha_pedido` date default NULL,
+  `fecha_pago` date default NULL,
+  `fecha_envio` date default NULL,
+  `fecha_entrega` date default NULL,
+  `cantidad` int(3) default NULL,
+  `pagado` tinyint(1) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `pedidos` */
+
+/*Table structure for table `productos` */
+
+DROP TABLE IF EXISTS `productos`;
+
+CREATE TABLE `productos` (
+  `id` int(12) NOT NULL auto_increment,
+  `nombre` varchar(256) collate utf8_spanish_ci default NULL,
+  `descripcion` text collate utf8_spanish_ci,
+  `precio` double default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `productos` */
+
+/*Table structure for table `usuarios` */
+
+DROP TABLE IF EXISTS `usuarios`;
+
+CREATE TABLE `usuarios` (
+  `id` int(12) NOT NULL auto_increment,
+  `user` varchar(45) collate utf8_spanish_ci default NULL,
+  `pass` varchar(450) collate utf8_spanish_ci default NULL,
+  `tipo` char(1) collate utf8_spanish_ci default 'C',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `usuarios` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
