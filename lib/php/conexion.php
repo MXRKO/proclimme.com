b9897c435bda84941a47c70b3a8a26413dfa1e7b
Marco Antonio Lozada <zero_could00@hotmail.com>
2012-06-18 10:55:29 -0500

    actualizacion pau portal

--- lib/php/conexion.php
+++ lib/php/conexion.php
@@ -1,6 +1,6 @@
 <?
 	mysql_connect("localhost","root");
 	mysql_select_db("proclimme_db");
-	/*mysql_connect("localhost","u157790_website","BCA110928uh4");
-	mysql_select_db("u157790_website");*/
+	/*mysql_connect("proclimmesql.db.9243612.hostedresource.com","proclimmeSQLread","readSQL_2012");
+	mysql_select_db("proclimmesql")*/
 ?>
